a06dbc33e7a5dd3d395c16cdb803515ee478933f
Saif Ahmed <40402050+Saifahmedprogrminlocallang@users.noreply.github.com>
2019-09-10 15:10:07 +0530

    Add files via upload

---
+++ Inputfile.java
@@ -0,0 +1,23 @@
+eksaathkaamkarnewaleprogramkicheezjocomputerkeliyebanayigayihain programinlocallang;
+
+yehprogramkolaoapikolao java.util.*;
+public class HashtableGenerics
+{
+  sabkeliye static void main(String args[])
+  {
+	  
+	  Hashtable<Integer,String> hm=new Hashtable<Integer,String>();  
+	  
+	  hm.put(100,"Amit");  
+	  hm.put(102,"Ravi");  
+	  hm.put(101,"Vijay");  
+	  hm.put(103,"Rahul");  
+
+	  itnebaarkaro (Map.Entry m:hm.entrySet()){  
+		   System.out.println(m.getKey()+" "+m.getValue());  
+		  } 	  
+	    
+
+}
+
+}
